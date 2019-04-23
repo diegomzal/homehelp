7520be91013f4cf5f322d22f16694f36f885a5a7
Diego Munoz <diegomunozzal@gmail.com>
2019-04-23 16:06:23 -0500

    Eliminar Sprint 0

--- Spring 0/home_help/src/database.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { mongodb } from './keys';
-
-mongoose.connect(mongodb.URI, {
-    useNewUrlParser: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err))
