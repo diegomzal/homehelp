fada565ff9097cffa23b8d957c195c88d3afe23b
diegomzal <20150931@aloe.ulima.edu.pe>
2019-07-07 17:34:50 -0500

    agregado paypal editado

---
+++ src/paypal.js
@@ -0,0 +1,44 @@
+const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
+const request = new checkoutNodeJssdk.orders.OrdersCreateRequest();
+request.headers["prefer"] = "return=representation";
+
+function client() {
+    return new checkoutNodeJssdk.core.PayPalHttpClient(environment());
+}
+
+function environment() {
+    let clientId = process.env.PAYPAL_CLIENT_ID
+    let clientSecret = process.env.PAYPAL_CLIENT_SECRET
+
+    return new checkoutNodeJssdk.core.SandboxEnvironment(
+        clientId, clientSecret
+    );
+}
+
+async function prettyPrint(jsonData, pre=""){
+    let pretty = "";
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+    for (let key in jsonData){
+        if (jsonData.hasOwnProperty(key)){
+            if (isNaN(key))
+              pretty += pre + capitalize(key) + ": ";
+            else
+              pretty += pre + (parseInt(key) + 1) + ": ";
+            if (typeof jsonData[key] === "object"){
+                pretty += "\n";
+                pretty += await prettyPrint(jsonData[key], pre + "    ");
+            }
+            else {
+                pretty += jsonData[key] + "\n";
+            }
+
+        }
+    }
+    return pretty;
+}
+
+
+
+module.exports = {client: client, prettyPrint:prettyPrint};
